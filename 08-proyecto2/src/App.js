54c488724cd9e56a4b5190bfad3f2444917b7ed9
Alejandro Valenzuela Bovet <ale.j1310@outlook.com>
2022-12-27 01:14:37 -0300

    2do Proyecto

---
+++ 08-proyecto2/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Misrutas } from './Router/Misrutas';
+
+
+function App() {
+  return (
+    <div className='layout'>
+      
+    <Misrutas/>
+
+    </div>
+  );
+}
+
+export default App;
